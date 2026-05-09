65bba1a0ed0281a86a06d528d6b2683f9303471a
Abdullah773903529 <Abdullah.mohammed.shamsan@example.com>
2026-05-08 23:32:36 -0700

    Regulation costs and raising part of the NIFI assignment

--- Ahmed Elsaid Assignment/nifi-assignment/scripts/generator.py
+++ Ahmed Elsaid Assignment/nifi-assignment/scripts/generator.py
@@ -5,7 +5,7 @@ import os
 from datetime import datetime
 
 # المسار الصحيح (مرتبط مع Docker)
-output_dir = r"C:\Users\Admin\Desktop\A1 Assignment\Ahmed Elsaid Assignment\nifi-assignment\data_input"
+output_dir = r"C:\Users\Admin\Desktop\Dokcer\lab_data"
 os.makedirs(output_dir, exist_ok=True)
 
 names = ["Ali", "Ahmed", "Sara", "Mona", "Khaled"]
@@ -13,7 +13,7 @@ cities = ["Sanaa", "Aden", "Taiz", "Ibb"]
 
 def generate_record():
     return {
-        "id": random.randint(1, 5),  # duplicates
+        "id": random.randint(1, 1000),  # duplicates
         "name": random.choice(names),
         "city": random.choice(cities),
         "amount": random.choice([100, 200, None, 300]),  # missing values
@@ -26,11 +26,12 @@ def generate_record():
 while True:
     data = [generate_record() for _ in range(5)]
 
-    file_name = f"data_{int(time.time())}.json"
+    # file_name = f"Transaction_{int(time.time())}.json"
+    file_name = f"Transaction_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.json"
     file_path = os.path.join(output_dir, file_name)
 
     with open(file_path, "w") as f:
         json.dump(data, f, indent=2)
 
-    print(f"Generated: {file_name}")
-    time.sleep(5)
+    print(f"Transaction_:{file_name}")
+    time.sleep(3)
